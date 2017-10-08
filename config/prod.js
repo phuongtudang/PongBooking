f5e3b74bb02cd5c8f71d96623b8913bf43bc909d
Phuong Dang <phuongdang@admins-MacBook-Air-2.local>
2017-10-08 14:20:30 +0700

    fix key file

---
+++ config/prod.js
@@ -0,0 +1,9 @@
+module.exports = {
+  consumerKey: process.env.TWITTER_CLIENT_ID,
+  consumerSecret: process.env.TWITTER_CLIENT_SECRET,
+  mongoURI: process.env.MONGO_URI,
+  cookieKey: process.env.COOKIE_KEY,
+  yelpClientID: process.env.YELP_CLIENT_ID,
+  yelpClientSecret: process.env.YELP_CLIENT_SECRET,
+  yelpToken: process.env.YELP_TOKEN
+};
